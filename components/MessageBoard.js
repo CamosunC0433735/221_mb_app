7fd0d808b4d3d787e9816c78d7e5d9bfb8447984
David Abram-Krismer <C0433735@online.camosun.ca>
2023-01-31 10:41:38 -0800

    Updated to work with api

--- components/MessageBoard.js
+++ components/MessageBoard.js
@@ -1,23 +1,34 @@
 import NewMessageForm from '../components/NewMessageForm'
 import MessageTable from '../components/MessageTable'
 import { useState } from 'react';
+import axios from 'axios';
 
 
-const MessageBoard = ({ }) => {
+const MessageBoard = ({ jsonData }) => {
 
-    const [myMessages, setMessages] = useState(
-        [
-            { id: 0, name: "David", messageTxt: "Hello world" },
-            { id: 1, name: "Paul", messageTxt: "Hi David" },
-            { id: 2, name: "George", messageTxt: "I love writing messages to my imaginary friends" }
-        ]);
+    const [myMessages, setMessages] = useState(jsonData);
 
     useState()
 
-    const addNewMessage = (values) => {
-        values.id = myMessages.length;
-        console.log(values);
-        setMessages([values, ...myMessages]);
+    const addNewMessage = async (values) => {
+
+        let msg = {"name": values.name, "messageTxt": values.messageTxt};
+        console.log(msg);
+
+        try{
+            axios.post('http://10.21.75.113:3004/api/messages', msg)
+            .then(
+                function(res){
+                    console.log(res);
+                    setMessages([values, ...myMessages]);
+                }).catch(function (error) {
+                    console.log(error);
+                });
+
+        } catch(err){
+
+        }
+        
     }
 
     return(
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,25 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import MessageBoard from '../components/MessageBoard'
 import { Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
-export default function Home() {
+export async function getStaticProps() {
+    let jsonData;
+    try {
+      const { data } = await
+      axios.get('http://localhost:3004/api/messages');
+      jsonData = data;
+    } catch (error) {
+      console.log('API Error: ' + error);
+    }
+    return {
+      props: {
+      jsonData
+      }
+    }
+  }
+
+export default function Home({jsonData}) {
   return (
     <>
       <Head>
@@ -19,7 +36,7 @@ export default function Home() {
             <Row className="justify-content-center">
                 <Col lg={8}>
                     <Header />
-                    <MessageBoard />
+                    <MessageBoard jsonData={jsonData}/>
                     <Footer />
                 </Col>
             </Row>
